d555aad3834d1f5d6f72bff9850d44bba1ebe576
Kit Isaev <14875494+nikis05@users.noreply.github.com>
2025-08-19 20:05:24 +0000

    Remove unneeded dependencies and add default color

--- render-server/src/render/colors.rs
+++ render-server/src/render/colors.rs
@@ -1,3 +1,5 @@
 use phf::phf_map;
 
-pub static COLORS: phf::Map<u8, image::Rgb<u8>> = phf_map! {};
+pub static COLORS: phf::Map<u8, image::Rgb<u8>> = phf_map! {
+    0u8 => image::Rgb([0, 0, 0])
+};
