a17c62ac06a79d8342ffcd91f61800db28c33c1a
Kit Isaev <14875494+nikis05@users.noreply.github.com>
2025-09-28 07:38:05 +0000

    Fix openLink logic

--- app/src/utils/openLink.ts
+++ app/src/utils/openLink.ts
@@ -1,7 +1,9 @@
+import { openLink as tgOpenLink } from "@telegram-apps/sdk-react";
+
 export function openLink(url: string) {
   if (import.meta.env.DEV) {
     window.open(url);
   } else {
-    openLink(url);
+    tgOpenLink(url);
   }
 }
