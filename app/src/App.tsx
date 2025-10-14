2f77d6985353467004286d0722d3bb3816784473
Kit Isaev <14875494+nikis05@users.noreply.github.com>
2025-10-14 22:19:27 +0000

    Resolve issues with cloud storage

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,108 @@
-import { retrieveLaunchParams } from "@telegram-apps/sdk-react";
-import { AppRoot } from "@telegram-apps/telegram-ui";
-import { FC, useMemo } from "react";
+import {
+  getCloudStorageItem,
+  retrieveLaunchParams,
+} from "@telegram-apps/sdk-react";
+import { AppRoot, Spinner } from "@telegram-apps/telegram-ui";
+import { FC, useEffect, useMemo, useState } from "react";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 import { Nav } from "./components/Nav";
 import { TosModal } from "./components/TosModal";
-import { useModal } from "./components/Modal";
+import { useIsMounted } from "usehooks-ts";
+import { captureException } from "@sentry/react";
+import { initEditor } from "./initEditor";
+import { Editor } from "./model/editor";
+import { EditorProvider } from "./model/editor/useEditor";
+
+export const TOS_STORAGE_KEY = "@pixel-canvas/tos";
+export const EDITOR_STORAGE_KEY = "@pixel-canvas/editor";
 
 export const App: FC = () => {
   const lp = useMemo(() => retrieveLaunchParams(), []);
-  const tosModal = useModal();
+
+  const isCloudStorageAvailable = getCloudStorageItem.isAvailable();
+
+  const isMounted = useIsMounted();
+
+  const [cloudStorageData, setCloudStorageData] = useState<
+    | { available: false }
+    | { available: true; acceptedTosVersion: string; editorData: string | null }
+    | null
+  >(null);
+
+  useEffect(() => {
+    if (import.meta.env.DEV) {
+      const version = localStorage.getItem(TOS_STORAGE_KEY);
+      const editorData = localStorage.getItem(EDITOR_STORAGE_KEY);
+      setCloudStorageData({
+        available: true,
+        acceptedTosVersion: version ?? "",
+        editorData,
+      });
+    } else {
+      if (isCloudStorageAvailable) {
+        Promise.all([
+          getCloudStorageItem(TOS_STORAGE_KEY, { timeout: 2000 }),
+          getCloudStorageItem(EDITOR_STORAGE_KEY, { timeout: 2000 }),
+        ])
+          .then(([version, editorData]) => {
+            if (isMounted())
+              setCloudStorageData({
+                available: true,
+                acceptedTosVersion: version,
+                editorData: editorData === "" ? null : editorData,
+              });
+          })
+          .catch(captureException);
+      } else {
+        setCloudStorageData({ available: false });
+      }
+    }
+  }, [setCloudStorageData, isCloudStorageAvailable, isMounted]);
+
+  const editor = useMemo<Editor | null>(() => {
+    if (cloudStorageData == null) return null;
+    const editor = initEditor(
+      cloudStorageData.available ? cloudStorageData.editorData : null
+    );
+    return editor;
+  }, [cloudStorageData]);
+
   return (
     <AppRoot
       appearance="dark"
       platform={["macos", "ios"].includes(lp.tgWebAppPlatform) ? "ios" : "base"}
     >
-      <HashRouter>
-        <Nav>
-          <Routes>
-            {routes.map((route) => (
-              <Route key={route.path} {...route} />
-            ))}
-            <Route path="*" element={<Navigate to="/editor" />} />
-          </Routes>
-        </Nav>
-      </HashRouter>
-      <TosModal handle={tosModal} />
+      {cloudStorageData != null && editor != null ? (
+        <>
+          <HashRouter>
+            <EditorProvider editor={editor}>
+              <Nav>
+                <Routes>
+                  {routes.map((route) => (
+                    <Route key={route.path} {...route} />
+                  ))}
+                  <Route path="*" element={<Navigate to="/editor" />} />
+                </Routes>
+              </Nav>
+            </EditorProvider>
+          </HashRouter>
+          <TosModal
+            acceptedTosVersion={
+              cloudStorageData.available
+                ? {
+                    available: true,
+                    version: cloudStorageData.acceptedTosVersion,
+                  }
+                : { available: false }
+            }
+          />
+        </>
+      ) : (
+        <div className="pt-5 flex justify-center items-center">
+          <Spinner size="l" />
+        </div>
+      )}
     </AppRoot>
   );
 };
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -9,10 +9,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { publicUrl } from "./utils/publicUrl.ts";
 import { App } from "./App.tsx";
 import "./index.css";
-import { initEditor } from "./initEditor.ts";
-import { EditorProvider } from "./model/editor/useEditor.tsx";
-import { captureException, init as initSentry } from "@sentry/react";
-import { getCloudStorageItem } from "@telegram-apps/sdk-react";
+import { init as initSentry } from "@sentry/react";
 
 function loadEnvVar(name: string) {
   const value = import.meta.env[name] as string | undefined;
@@ -23,61 +20,33 @@ function loadEnvVar(name: string) {
 export const STORE_ADDRESS = loadEnvVar("VITE_STORE_ADDRESS");
 export const API_URL = loadEnvVar("VITE_API_URL");
 export const RENDER_URL = loadEnvVar("VITE_RENDER_URL");
-export const TOS_STORAGE_KEY = "@pixel-canvas/tos";
-export const TOS_VERSION = "1";
-export let ACCEPTED_TOS_VERSION: string | null;
 
-async function main() {
-  initSentry({
-    dsn: "https://41577d78bcef62be195e4796d03840ac@o4504381711056896.ingest.us.sentry.io/4510161823596544",
-    sendDefaultPii: true,
-  });
+initSentry({
+  dsn: "https://41577d78bcef62be195e4796d03840ac@o4504381711056896.ingest.us.sentry.io/4510161823596544",
+  sendDefaultPii: true,
+});
 
-  const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-  try {
-    initMiniApp();
+try {
+  initMiniApp();
 
-    const isCloudStorageAvail = getCloudStorageItem.isAvailable();
-
-    console.log("Cloud storage support: ", getCloudStorageItem.isSupported());
-    console.log("Cloud storage availability: ", isCloudStorageAvail);
-
-    const editor = await initEditor(isCloudStorageAvail);
-
-    console.log("Initialized editor");
-
-    ACCEPTED_TOS_VERSION = import.meta.env.DEV
-      ? (localStorage.getItem(TOS_STORAGE_KEY) ?? "")
-      : isCloudStorageAvail
-        ? await getCloudStorageItem(TOS_STORAGE_KEY)
-        : null;
-
-    console.log("Loaded TOS version");
-
-    console.log("Preparing for render");
-
-    root.render(
-      <StrictMode>
-        <ErrorBoundary>
-          <TonConnectUIProvider
-            manifestUrl={
-              (import.meta.env.VITE_TC_MANIFEST_URL as string | undefined) ??
-              publicUrl("tonconnect-manifest.json")
-            }
-          >
-            <EditorProvider editor={editor}>
-              <App />
-            </EditorProvider>
-          </TonConnectUIProvider>
-        </ErrorBoundary>
-      </StrictMode>
-    );
-    console.log("Initial render complete");
-  } catch (e) {
-    console.log(e);
-    root.render(<EnvUnsupported />);
-  }
+  root.render(
+    <StrictMode>
+      <ErrorBoundary>
+        <TonConnectUIProvider
+          manifestUrl={
+            (import.meta.env.VITE_TC_MANIFEST_URL as string | undefined) ??
+            publicUrl("tonconnect-manifest.json")
+          }
+        >
+          <App />
+        </TonConnectUIProvider>
+      </ErrorBoundary>
+    </StrictMode>
+  );
+  console.log("Initial render complete");
+} catch (e) {
+  console.log(e);
+  root.render(<EnvUnsupported />);
 }
-
-main().catch(captureException);
